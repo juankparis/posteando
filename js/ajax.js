20f991fc400d716018bf67ef51f9a4a9b5e19dbf
juan carlos <juan.k.paris@gmail.com>
2015-06-10 15:50:57 -0500

    ejeplo terminado de una peticion ajax del mismo sevidor

--- js/ajax.js
+++ js/ajax.js
@@ -2,9 +2,9 @@ $(function(){
 	//console.log('hola soy ajax');
 					//este es un ejemplo con ajax para el propio servidor
 					//se esta cargando la pagina logo_ajax.html 
-	$.get("logo_ajax.html", function(cualquier){
-		$("footer").append(cualquier);
-	});
+	// $.get("logo_ajax.html", function(cualquier){
+	// 	$("footer").append(cualquier);
+	// });
 
 	
 
