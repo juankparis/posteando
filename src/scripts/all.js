7b31995575afcf45b53c2a0466499ca5a4f1e9cf
juankparis <juan.k.paris@gmail.com>
2016-02-03 21:59:14 -0500

    src achivos a comprimir

---
+++ src/scripts/all.js
@@ -0,0 +1,88 @@
+var $ = require('jquery');
+
+var $form = $("#formulario"),
+	$titulo= $("#titulo"),
+	$url = $("#url"),
+	$tag = $("#tag"),
+	$autor = $("#autor"),
+	$boton = $("#mostrar-form"),
+	$list = $("#contenido"),
+	$post = $(".item").first(),
+	$video =$("#video");
+
+$(document).on("ready", function inicio () {
+	//llamadas a los modulos 
+	var loadCSS = require('./lib/loadCSS'); 
+	loadCSS('https://fonts.googleapis.com/css?family=Roboto+Slab|Oleo+Script');
+	
+	var geoposition = require('./lib/geoposition');
+	geoposition();
+
+	var peticionsever = require('./lib/peticionsever');
+	peticionsever();
+
+	var weather = require('./lib/weather');
+	weather();
+
+	//eventos 
+	$boton.on( "click", mostrarFormulario);
+	$form.on("submit", agregarpost);
+
+	////////////local storage ///////////
+	///localStorage.autosave="1" ///en consola//
+	// if (localStorage.getItem('autosave')) {
+		//si la variable existe ponga lo de sessionS.. en 
+		$titulo.val(sessionStorage.getItem('titulo'));
+		$url.val(sessionStorage.getItem('url'));
+		$tag.val(sessionStorage.getItem('tag'));
+		$autor.val(sessionStorage.getItem('autor'));
+	// }
+
+	var id = setInterval(function(){
+		sessionStorage.setItem('titulo', $titulo.val());
+		sessionStorage.setItem('url', $url.val());
+		sessionStorage.setItem('tag', $tag.val());
+		sessionStorage.setItem('autor', $autor.val());
+	}, 1000);
+
+	////////////////GEO///////////////
+
+});
+
+function mostrarFormulario () {
+	//forma fara ocultar el formulario
+	$form.slideToggle("slow");
+	//esconder todos los post
+	$list.slideToggle("slow");
+	//esconder el video que sale en resoluciones altas
+	$video.slideToggle("slow");
+	//para que no se salte la pagina 
+	return false;
+}
+function agregarpost () {
+	//e.preventDefault();
+	//e.stopPropagation();
+	var url = $url.val(),
+		titulo = $titulo.val(),
+		tag = $tag.val(),
+		autor = $autor.val(),
+		$clone = $post.clone();
+	
+	$clone.find(".titulo_item a").text(titulo).attr("href", url);
+	$clone.find(".datos_item a").text(tag).attr("href", url);
+	$clone.find(".autor_item a").text(autor).attr("href", url);
+	// esconder clone como en css
+	$clone.hide();
+	// agregacion
+	$list.prepend($clone);
+	// animacion al clone
+	$clone.fadeIn();
+	//vover a mostrar los post (escondidos arriba)
+	mostrarFormulario ();
+	//borra o setea los campos
+	$titulo.val('');
+	$url.val('');
+	$tag.val('');
+	$autor.val('');
+	return false;
+}
